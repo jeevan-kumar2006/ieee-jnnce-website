ae9a8f574895f0437654d14011cf75021b11c365
Jeevan Kumar A <jeevankumar@jnnce.ac.in>
2026-04-30 21:42:01 +0530

    Add Tailwind CSS configuration file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#020617',
+      },
+    },
+  },
+  plugins: [],
+}
