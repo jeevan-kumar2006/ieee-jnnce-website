6938f2907d45f05e9389dc67b78c419524d222e7
Jeevan Kumar A <jeevankumar@jnnce.ac.in>
2026-04-30 21:46:11 +0530

    Add Event and ContactData interfaces

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Event {
+  id: number;
+  title: string;
+  date: string;
+  desc: string;
+  tag: string;
+}
+
+export interface ContactData {
+  name: string;
+  email: string;
+  message: string;
+}
