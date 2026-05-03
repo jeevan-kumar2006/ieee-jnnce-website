a26e23b02445bceb8a6665d71c77d7f1946e294e
Jeevan Kumar A <jeevankumar@jnnce.ac.in>
2026-05-03 20:16:42 +0530

    Add background color to DotGridBackground component

--- frontend/src/components/DotGridBackground.tsx
+++ frontend/src/components/DotGridBackground.tsx
@@ -1,6 +1,6 @@
 export default function DotGridBackground() {
   return (
-    <div className="fixed inset-0 z-0">
+    <div className="fixed inset-0 z-0 bg-[#f3f3f3]">
       <div className="dot-grid w-full h-full">
         {Array.from({ length: 400 }).map((_, i) => (
           <div key={i} className="dot-cell"></div>
